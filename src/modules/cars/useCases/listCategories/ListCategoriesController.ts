df045aa518ec1421511a0eec2ace9191e9502d53
Rickson <rickson.simoes@hotmail.com>
2021-07-14 21:55:05 -0300

    :hammer: :wrench: Adicionando a injecao de dependencias para o resto dos useCases

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
-
   async handle(request: Request, response: Response): Promise<Response> {
-    const listCategories = await this.listCategoriesUseCase.execute();
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
 
-    return response.status(200).json(listCategories);
+    const listCategories = await listCategoriesUseCase.execute();
+
+    return response.json(listCategories);
   }
 }
 
