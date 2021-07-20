f8513fdc5fdb729cb8440d70c1f1e559722d1247
Rickson <rickson.simoes@hotmail.com>
2021-07-20 18:48:01 -0300

    :hammer: :wrench: Criacao de usuarios/mgirations/implementations/usecases

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private createUser: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    username,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    await this.createUser.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
